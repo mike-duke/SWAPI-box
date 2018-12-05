1d44420ee1c703f30241019383fee6cf02e36ff0
Cody-Price <ctprice11@gmail.com>
2018-12-05 15:00:46 -0700

    Add numeralized episode method

--- src/Components/ScrollingText/ScrollingText.js
+++ src/Components/ScrollingText/ScrollingText.js
@@ -2,13 +2,19 @@ import React from 'react';
 import './ScrollingText.scss';
 
 const ScrollingText = ({ crawl, title, episode }) => {
+
+  const numeralizeEpisode = (episodeNumber) => {
+    const numeralArray = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII'];
+    return numeralArray[episodeNumber - 1];
+  }
+
   return (
     <section className="scrolling-text">
       <div className="fade"></div>
       <article className="star-wars">
         <div className="crawl">
           <div className="title">
-            <p>{`Episode ${episode}`}</p>
+            <p>{`Episode ${numeralizeEpisode(episode)}`}</p>
             <h1>{title}</h1>
           </div>
           <p className="crawl-text">{crawl}</p>
