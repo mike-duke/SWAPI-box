cec14758797bd6daa9fb211a9955d33578f4166f
Derek Hanciles <drhanciles@icloud.com>
2018-12-05 16:44:46 -0700

    Test number two in app is implemented correctly and is passing

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -5,28 +5,61 @@ import {shallow} from 'enzyme';
 describe('App', () => {
   let mockRandomCrawl
   let wrapper
+  let mockUrl
+  let mockResponse
+  let mockFetchFilms
+  let mockSwapiFetch
 
   describe('componentDidMount', () => {
+    beforeEach(() => {
+      wrapper = shallow(<App />)
+      mockUrl = 'https://swapi.co/api/'
+      mockResponse = {
+        "people": "https://swapi.co/api/people/",
+        "planets": "https://swapi.co/api/planets/",
+        "films": "https://swapi.co/api/films/",
+        "species": "https://swapi.co/api/species/",
+        "vehicles": "https://swapi.co/api/vehicles/",
+        "starships": "https://swapi.co/api/starships/"
+    }
+      mockFetchFilms =  jest.fn().mockImplementation(() => {
+        Promise.resolve({
+          json: () => Promise.resolve(mockResponse)
+        })
+      })
+      window.fetch = jest.fn().mockImplementation(()=> {
+        return Promise.resolve({
+          json: () => Promise.resolve(mockResponse)
+        })
+      })
+    })
+
     it('should call our fetch method with the correct parameters', () => {
-      // Setup
-      // Execution
-      // Expectation
+      const expected = mockUrl
+      
+      wrapper.instance().componentDidMount()
+
+      expect(window.fetch).toHaveBeenCalledWith(expected)
     })
 
-    it('calls fetchFilms when a promise has been returned from our fetch method', () => {
-      // Setup
-      // Execution
-      // Expectation
+    it('calls fetchFilms with the correct params', async () => {
+      const mockUrl = 'https://swapi.co/api/films/'
+      const expected = mockUrl
+      wrapper.instance().fetchFilms = mockFetchFilms
+
+      await wrapper.instance().componentDidMount()
+
+      expect(mockFetchFilms).toHaveBeenCalledWith(expected)
     })
 
-    it('should update randomCrawl in state after our fetch call has been made successfully', () => {
+    it.skip('should update randomCrawl in state after our fetch call has been made successfully', () => {
       // Setup
       // Execution
       // Expectation
     
     })
 
-    it('should set an error if our fetch fails', () => {
+    it.skip('should set an error if our fetch fails', () => {
       // Setup
       // Execution
       // Expectation
