1c80830c551bc62064ecf3af4220e4efe10c91fb
Derek Hanciles <drhanciles@icloud.com>
2018-12-05 15:02:16 -0700

    Blocks that will test componentDidMount method have been implemented

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,9 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import {shallow} from 'enzyme'; 
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  let mockRandomCrawl
+  let wrapper
+
+  describe('componentDidMount', () => {
+    it('should call our fetch method with the correct parameters', () => {
+      // Setup
+      // Execution
+      // Expectation
+    })
+
+    it('calls fetchFilms when a promise has been returned from our fetch method', () => {
+      // Setup
+      // Execution
+      // Expectation
+    })
+
+    it('should update randomCrawl in state after our fetch call has been made successfully', () => {
+      // Setup
+      // Execution
+      // Expectation
+    
+    })
+
+    it('should set an error if our fetch fails', () => {
+      // Setup
+      // Execution
+      // Expectation
+
+    })
+  })
+})
+
