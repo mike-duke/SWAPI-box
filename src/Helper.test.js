765480080dcf055f999b54c5548d20dbc45a24ed
Derek Hanciles <drhanciles@icloud.com>
2018-12-08 15:02:24 -0700

    Test testing if our filmCleaner method returns an object in the correct format has been implemented and is now passing

--- src/Helper.test.js
+++ src/Helper.test.js
@@ -1,4 +1,6 @@
 import * as Helper from './Helper.js'; 
+import mockData from './mockData.js'
+
 
 describe('Helper', () => {
   describe('filmCleaner', () => {
@@ -6,5 +8,21 @@ describe('Helper', () => {
 
     expect(Helper.filmCleaner()).toBe(undefined)
    })
+
+   it('should return an object in specified format', () => {
+     const films = mockData.films
+     const randomIndex = 0 
+     const expected =  {
+      crawl: films.results[randomIndex].opening_crawl,
+      title: films.results[randomIndex].title,
+      episode: films.results[randomIndex].episode_id
+    }
+
+    Math.random = jest.fn().mockImplementation(() => 0)
+    const results = Helper.filmCleaner(films)
+
+    expect(results).toEqual(expected)
+   })
+
   })
 })
