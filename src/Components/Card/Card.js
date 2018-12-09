4ff1be6574f75587c351ade7093f4853a891a95d
Mike Duke <aithon@gmail.com>
2018-12-09 12:59:23 -0700

    Refactor Card component to have separate functions for each card type

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -4,53 +4,65 @@ import './Card.scss';
 const Card = ({card}) => {
   let displayedCard;
   if (Object.keys(card).includes('species')) {
-    displayedCard = (
-      <div>
-        <h3>{card.name}</h3>
-        <div className='image-div'>
-          <img src={card.image} className='image' alt={card.name}/>
-        </div>
-        <p>Species: {card.species}</p>
-        <p>Homeworld: {card.homeworld}</p>
-        <p>Homeworld population: {card.homeworldPop}</p>
-        <button className='fav-btn' onClick={
-          (e) => e.target.classList.toggle('active')
-        }>Favorite</button>
-      </div>
-    )
+    displayedCard = peopleCard(card);
   } else if (Object.keys(card).includes('model')) {
-    displayedCard = (
-      <div>
-        <h3>{card.name}</h3>
-        <p>Model: {card.model}</p>
-        <p>Class: {card.class}</p>
-        <p>Available passengers: {card.numberOfPassengers}</p>
-        <button className='fav-btn' onClick={
+    displayedCard = vehicleCard(card);
+  } else if (Object.keys(card).includes('terrain')) {
+    displayedCard = planetCard(card);
+  }
+
+  return (
+    <div className='card'>
+      {displayedCard}
+      <button className='fav-btn' onClick={
           (e) => e.target.classList.toggle('active')
         }>Favorite</button>
+    </div>
+  )
+}
+
+const peopleCard = (card) => {
+  return (
+    <div>
+      <h3>{card.name}</h3>
+      <div className='image-div'>
+        <img src={card.image} className='image' alt={card.name}/>
       </div>
-    )
-  } else if (Object.keys(card).includes('terrain')) {
-    displayedCard = (
-      <div>
-        <h3>{card.name}</h3>
+      <p>Species: {card.species}</p>
+      <p>Homeworld: {card.homeworld}</p>
+      <p>Homeworld population: {card.homeworldPop}</p>
+    </div>
+  )
+}
+
+const vehicleCard = (card) => {
+  return (
+    <div>
+      <h3>{card.name}</h3>
+      <p>Model: {card.model}</p>
+      <p>Class: {card.class}</p>
+      <p>Available passengers: {card.numberOfPassengers}</p>
+    </div>
+  )
+}
+
+const planetCard = (card) => {
+  return (
+    <div>
+      <h3>{card.name}</h3>
         <p>Population: {card.population}</p>
         <p>Climate: {card.climate}</p>
         <p>Terrain: {card.terrain}</p>
-        <p>Residents: </p><ul className='residents-list'>
+        <p>Residents: </p>
+        <ul className='residents-list'>
           {
             card.residents.map(resident => {
               return <li key={resident}>{resident}</li>
             })
           }
         </ul>
-        <button className='fav-btn' onClick={
-          (e) => e.target.classList.toggle('active')
-        }>Favorite</button>
-      </div>
-    )
-  }
-  return <div className='card'>{displayedCard}</div>
+    </div>
+  )
 }
 
 export default Card;
